36c9d75f4ee61da8d1a7178e7caa6836c1ce0985
ivan silva alves <ivan.ssa@gmail.com>
2024-09-02 17:27:09 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const projects = document.querySelectorAll(".project");
+
+    projects.forEach(project => {
+        project.classList.add("fade-in");
+    });
+});
+
+// Código para qualquer interação adicional no futuro
